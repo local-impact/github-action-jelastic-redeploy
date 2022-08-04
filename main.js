aa66ea04bb65db705ba26c6820892085b4d193f7
Martin Maillard <git@martinmaillard.com>
2022-08-04 14:47:59 +0200

    Add working action

---
+++ main.js
@@ -0,0 +1,39 @@
+const core = require('@actions/core');
+const httpm = require('@actions/http-client');
+
+async function main() {
+  const jelasticUrl = core.getInput('jelastic_url');
+  const jelasticToken = core.getInput('jelastic_token');
+  const envName = core.getInput('jelastic_env');
+  const version = core.getInput('version');
+  const deployDelay = core.getInput('deploy_delay');
+  const requestTimeout = Number(core.getInput('request_timeout'));
+
+  const actionPath = 'environment/control/rest/redeploycontainersbygroup';
+
+  core.info(`Deploying ${envName} on ${jelasticUrl} to version ${version} (delay=${deployDelay})`);
+
+  const params = new URLSearchParams({
+    session: jelasticToken,
+    envName,
+    tag: version,
+    nodeGroup: 'cp',
+    isSequential: true,
+    delay: deployDelay,
+  });
+  const url = new URL(`https://${jelasticUrl}/1.0/${actionPath}?${params}`);
+  const client = new httpm.HttpClient('', [], { socketTimeout: requestTimeout });
+  const response = await client.post(url);
+  const data = await response.readBody();
+  const jsonData = JSON.parse(data);
+
+  core.setOutput('response', data);
+
+  if (jsonData?.result !== 0) {
+    throw new Error(`Could not deploy: ${data}`);
+  }
+}
+
+main()
+  .catch((error) => core.setFailed(error.message));
+
